8658ddab9359df2c5530f6c45dbfe3900018408a
trung-doan <trung-doan@cybozu.vn>
2020-10-26 13:27:19 +0700

    Fix eslint

--- examples/js-edit/src/config-screen/index.ts
+++ examples/js-edit/src/config-screen/index.ts
@@ -11,30 +11,30 @@ import Editor from '../components/editor/index';
 
 import './index.css';
 
-const rootEl = document.getElementById("jsedit-config");
+const rootEl = document.getElementById('jsedit-config');
 
 const toobar = new Toolbar();
 const toolbarEl = toobar.render();
 rootEl.appendChild(toolbarEl);
 
 toolbarEl.addEventListener('save', () => {
-    console.log('save');
+  console.log('save');
 });
 
 toolbarEl.addEventListener('discard', () => {
-    console.log('discard');
+  console.log('discard');
 });
 
 toolbarEl.addEventListener('back-to-plugins', () => {
-    console.log('back-to-plugins');
+  console.log('back-to-plugins');
 });
 
 toolbarEl.addEventListener('help', () => {
-    console.log('help');
+  console.log('help');
 });
 
 toolbarEl.addEventListener('max-min-toggle', () => {
-    console.log('max-min-toggle');
+  console.log('max-min-toggle');
 });
 
 const contentEl = document.createElement('div');
