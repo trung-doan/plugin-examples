65ee21c8bfe881a27d524f2d47ac0ba445e56f2f
trung-doan <trung-doan@cybozu.vn>
2020-11-02 11:13:21 +0700

    remove unneeded function

--- examples/js-edit/src/customization/modal/index.ts
+++ examples/js-edit/src/customization/modal/index.ts
@@ -1,23 +1,5 @@
 import './index.css';
 
-export function createModalContent() {
-  const textbox = new kintoneUIComponent.Text();
-  const textboxEl = textbox.render();
-  textboxEl.id = 'input-value';
-
-  const div = document.createElement('div');
-  div.appendChild(textboxEl);
-  return div;
-}
-
-export function createButtonEl(value: string = 'Save') {
-  const buttonSave = new kintoneUIComponent.Button({
-    text: value
-  });
-
-  return buttonSave.render();
-}
-
 export function createModalContentRow(textbox: any, button: any) {
   const div = document.createElement('div');
   div.className = 'js-edit__modal__modal-container__content__row';
