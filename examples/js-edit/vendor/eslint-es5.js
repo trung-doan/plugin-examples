0e43aff90006885d4ab0f04fbb25db965c2bd5ae
phongnm-dev <p001842@PC-001842-1.local>
2021-06-09 09:26:44 +0700

    Update eslint rule

--- examples/js-edit/vendor/eslint-es5.js
+++ examples/js-edit/vendor/eslint-es5.js
@@ -1,6 +1,6 @@
 const recomendEslintRule = require('eslint/conf/eslint-recommended.js');
 const cybozuEs5Rules = require('@cybozu/eslint-config/lib/es5.js');
-
+const globalKintone = require('@cybozu/eslint-config/globals/kintone.js');
 Object.assign(cybozuEs5Rules.rules, {...recomendEslintRule.rules, ...cybozuEs5Rules.rules});
-
+Object.assign(cybozuEs5Rules, {...globalKintone});
 export default cybozuEs5Rules;
